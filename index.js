0e7823a82e21ca58c71bca07007fac98d1e2d387
Tyree Zacharopoulos <tyree@tyree.ca>
2023-03-07 14:09:08 -0800

    fix(Logging)[API-22]: Basic Logger
    Not working as expected

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const winston = require("winston");
 
-const enumerateErrorFormat = winston.format((info) => {
-	if (info instanceof Error) {
-		Object.assign(info, { message: info.stack });
-	}
-	return info;
-});
+// const enumerateErrorFormat = winston.format((info) => {
+// 	if (info instanceof Error) {
+// 		Object.assign(info, { message: info.stack });
+// 	}
+// 	return info;
+// });
 
 const Logger = winston.createLogger({
-	format: winston.format.combine(enumerateErrorFormat()),
-	transports: [
-		new winston.transports.Console({
-			stderrLevels: ["error"],
-		}),
-	],
+    transports: [
+        new winston.transports.Console()
+    ]
 });
 
 
